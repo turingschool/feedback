567b8f64db6fae2a3f442593b00afc6a8871efd1
Timothy Mee <tjmee90@gmail.com>
2015-09-10 10:51:22 -0600

    added fucntion to check if user_for is eligable to get a feedback when a feedback gets eligable to send, added project title to subject of feedback submission email

--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -1,10 +1,10 @@
 class SubmissionMailer < ApplicationMailer
   default :from => "Feedback@turing.io"
 
-  def send_submission(submission, user)
+  def send_submission(submission, user, title)
     @user_to  = user
     @feedback = submission
     mail( :to => @user_to.email,
-    :subject => "Project FeedBack")
+    :subject => "#{title} FeedBack")
   end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,5 @@
 class Submission < ActiveRecord::Base
-  belongs_to :invite, foreign_key: "token"
+  belongs_to :invite
   belongs_to :feedback_from, :class_name => User
   belongs_to :feedback_for, :class_name => User
 
@@ -8,8 +8,19 @@ class Submission < ActiveRecord::Base
   validates_presence_of :again
   validates :comments, :length => {in: 10..240}
   validates :peer_review_score, :inclusion => {in: -2..2}
+  after_save :check_to_send
+  scope :not_constructive, ->{ where(peer_review_score: -2) }
+  scope :constructive, ->{ where(peer_review_score: 2) }
 
-  scope :not_nice, ->{ where(peer_review_score: -2) }
+  def check_to_send
+    if peer_review_score == 2
+      check_user_to_score_count(feedback_for_id)
+    end
+  end
+
+  def project_title
+    invite.invite_set.title
+  end
 
   def self.options
     [
@@ -29,5 +40,14 @@ class Submission < ActiveRecord::Base
       'agree'             =>  1,
       'strongly_agree'    =>  2
     }[word]
+  end
+
+  private
+
+  def check_user_to_score_count(uid)
+    user = User.find(uid)
+    if user.peer_review_count >= 3
+      user.send_submission_email
+    end
   end
 end
