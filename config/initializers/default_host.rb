b4c3b5ab19f053b6ca0c81be158c3f5b75b8057e
Horace Williams <horace.d.williams@gmail.com>
2016-02-18 13:54:01 -0700

    set dat host

--- config/initializers/default_host.rb
+++ config/initializers/default_host.rb
@@ -1,4 +1,6 @@
 if Rails.env.development?
   ngrok_url = "https://484a5bf8.ngrok.io"
   Rails.application.routes.default_url_options[:host] = ngrok_url
+else
+Rails.application.routes.default_url_options[:host]= "https://turingfeedback.herokuapp.com"
 end
